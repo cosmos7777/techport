3c29b8801dda1bfa3df6df634cc0d72cc6fcd337
Andrey G <cosmos7777@yandex.ru>
2018-10-23 17:32:57 +0300

    4

--- src/test33.py
+++ src/test33.py
@@ -16,7 +16,8 @@ class LoginTest(unittest.TestCase):
         driver = self.driver
         driver.get("http://test1.techport.ru/katalog/products/holodilniki/dvuhkamernye/holodilnik-atlant-4008-020")
         # Нажимаем купить в 1 клик
-        driver.find_element_by_xpath("/html/body/div[2]/div[2]/main/div[2]/div[1]/div/div[1]/div[2]/div[3]/div[6]/div/div/ul/li[1]/a/span[2]").click()
+        driver.find_element_by_xpath("/html/body/div[2]/div[2]/main/div[2]/div[1]/div/div[1]/div[2]/div[3]/div[6]/div/div/ul/li[1]/a/span[1]").click()
+        #driver.find_element_by_xpath("/html/body/div[2]/div[2]/main/div[2]/div[1]/div/div[1]/div[2]/div[3]/div[6]/div/div/ul/li[1]/a/span[2]").click()
         # Заголовок "Купить в 1 клик" отображается
         driver.find_element_by_xpath("/html/body/div[2]/noindex/div/div[4]/div/div[1]/div/div/div/h1").is_displayed()
 
