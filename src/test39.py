4ff3dc2645de733636eaa568afcfba782eb5eb18
Andrey G <cosmos7777@yandex.ru>
2018-10-23 19:08:59 +0300

    4

--- src/test39.py
+++ src/test39.py
@@ -17,8 +17,6 @@ class Test(unittest.TestCase):
         driver.get("http://test1.techport.ru/katalog/products/holodilniki/dvuhkamernye/holodilnik-atlant-4008-020")
         driver.find_element_by_xpath("/html/body/div[2]/div[2]/main/div[2]/div[1]/div/div[1]/div[2]/div[3]/div[1]/div[2]/form/div/div[1]/div/div[3]/button").click()
         driver.find_element_by_xpath("/html/body/div[2]/div[2]/main/div[2]/div[1]/div/div[1]/div[2]/div[3]/div[1]/div[2]/form/div/div[1]/div/div[1]/button").click()
-
-
         driver.find_element_by_xpath("/html/body/div[2]/div[2]/main/div[2]/div[3]/div/div/div[2]").is_enabled()
 
 
