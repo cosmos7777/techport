6eeb04ba8b871063f7add4385e1860506e4aa5be
Andrey G <cosmos7777@yandex.ru>
2018-10-23 18:44:44 +0300

    4

--- src/test38.py
+++ src/test38.py
@@ -16,7 +16,7 @@ class Test(unittest.TestCase):
         driver = self.driver
         driver.get("http://test1.techport.ru/katalog/products/holodilniki/dvuhkamernye/holodilnik-atlant-4008-020")
         #driver.find_element_by_xpath("/html/body/div[2]/div[2]/main/div[2]/div[1]/div/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/table/tbody/tr[13]/td/a").click()
-        driver.find_element_by_xpath("/html/body/div[2]/div[2]/main/div[2]/div[1]/div/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/table/tbody/tr[13]/td").click()
+        driver.find_element_by_css_selector(".tcp-specification__link").click()
 
         cos = driver.find_element_by_xpath("/html/body/div[2]/div[2]/main/div[2]/div[3]/div/div/div[1]/div[1]/div/div[2]/div/table/tbody/tr[1]/td[1]")
         assert 'Общий полезный объем' in cos.text
