265ac332624dc9591d27a7a17cf5ebb20f9f8b16
Andrey G <cosmos7777@yandex.ru>
2018-10-24 14:29:04 +0300

    4

--- src/test33.py
+++ src/test33.py
@@ -6,7 +6,7 @@ class LoginTest(unittest.TestCase):
       #self.driver = webdriver.Firefox(executable_path='/geckodriver')
       self.driver = webdriver.Chrome(executable_path='/chromedriver')
       self.driver.maximize_window()
-      self.driver.implicitly_wait(10)
+      self.driver.implicitly_wait(30)
 
 
 
@@ -16,13 +16,13 @@ class LoginTest(unittest.TestCase):
         driver = self.driver
         driver.get("http://test1.techport.ru/katalog/products/holodilniki/dvuhkamernye/holodilnik-atlant-4008-020")
         # Нажимаем купить в 1 клик
-        driver.find_element_by_link_text("Купить в 1 клик").click()
+        #driver.find_element_by_link_text("Купить в 1 клик").click()
 
 
         #driver.find_element_by_css_selector("#page-section > div.item_wrapp > div.item_info > div > div.tcp-product.tcp-product_single.tcp-product_no_radius > div:nth-child(2) > div.tcp-col.tcp-col_lg-3.tcp-visible-lg > div.tcp-product-detail.tcp-product-detail_full.tcp-product-detail_bordered > div.tcp-product-detail__item.tcp-product-detail__item_pad-xs > div.tcp-product-specification.tcp-product-specification_inline > table > tbody > tr:nth-child(13) > td > a").click()
         #driver.find_element_by_xpath("//*[@id=page-section]/div[2]/div[1]/div/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/table/tbody/tr[13]/td/a").click()
         # Заголовок "Купить в 1 клик" отображается
-        driver.find_element_by_id("icon-modal-content").is_displayed()
+        #driver.find_element_by_id("icon-modal-content").is_displayed()
 
 
 
