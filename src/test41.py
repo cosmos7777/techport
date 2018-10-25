9e5d0c026d6e1d75d8b2d6b39c87fb0144d01e70
Andrey G <cosmos7777@yandex.ru>
2018-10-25 13:28:14 +0300

    5

--- src/test41.py
+++ src/test41.py
@@ -17,7 +17,7 @@ class Test(unittest.TestCase):
         driver.get("http://test1.techport.ru/katalog/products/holodilniki")
         # кликаем на фильтр "глубина" и выставляем значени 56 и 62
 
-        driver.find_element_by_id("tpf-227")
+        driver.find_element_by_css_selector("div#tpf-227 > div").click()
 
         #driver.find_element_by_xpath("/html/body/div[2]/div[2]/main/div[2]/div/div/div[1]/div/form/div[3]/div/div[7]/div[1]").click()
         form1 = driver.find_element_by_xpath("/html/body/div[2]/div[2]/main/div[2]/div/div/div[1]/div/form/div[3]/div/div[7]/div[2]/div/div[1]/div/input")
