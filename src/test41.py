4a42612819731c48ef74e603c9901457f128f2a0
Andrey G <cosmos7777@yandex.ru>
2018-10-25 15:26:18 +0300

    5

--- src/test41.py
+++ src/test41.py
@@ -5,7 +5,8 @@ from selenium import webdriver
 # test41 Проверка фильтров на странице - "От/до  и ползунок"
 class Test(unittest.TestCase):
     def setUp(self):
-        # self.driver = webdriver.Firefox(executable_path='/geckodriver')
+
+        #self.driver = webdriver.PhantomJS(executable_path='/phantomjs')
         self.driver = webdriver.Chrome(executable_path='/chromedriver')
         self.driver.maximize_window()
         self.driver.implicitly_wait(10)
@@ -16,14 +17,11 @@ class Test(unittest.TestCase):
         driver = self.driver
         driver.get("http://test1.techport.ru/katalog/products/holodilniki")
         # кликаем на фильтр "глубина" и выставляем значени 56 и 62
+        driver.find_element_by_css_selector("#tpf-227 > div:nth-child(1)").click()
 
 
-
-
-
-        driver.find_element_by_css_selector("div#tpf-227 > div").click()
-
-        driver.find_element_by_xpath("/html/body/div[2]/div[2]/main/div[2]/div/div/div[1]/div/form/div[3]/div/div[7]/div[1]").click()
+        #driver.find_element_by_css_selector("div#tpf-227 > div").click()
+        #driver.find_element_by_xpath("/html/body/div[2]/div[2]/main/div[2]/div/div/div[1]/div/form/div[3]/div/div[7]/div[1]").click()
         form1 = driver.find_element_by_xpath("/html/body/div[2]/div[2]/main/div[2]/div/div/div[1]/div/form/div[3]/div/div[7]/div[2]/div/div[1]/div/input")
         form2 = driver.find_element_by_xpath("/html/body/div[2]/div[2]/main/div[2]/div/div/div[1]/div/form/div[3]/div/div[7]/div[2]/div/div[2]/div/input")
         form1.clear()
